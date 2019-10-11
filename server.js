1011af1df97fb7de92b2aeb081ca46eef473eee6
Boxer-boxer <jorgedasilvarodrigues92@gmail.com>
2019-10-11 17:32:36 +0100

    new config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`Server is Running on ${PORT}`)	
